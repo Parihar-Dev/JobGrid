e195e8ea7bca003288ab517bd34612fe921089d6
Rahul Parihar <pariharr0203@gmail.com>
2025-07-04 03:59:32 +0530

    Resume Upload

--- JobGrid-Client/src/main.jsx
+++ JobGrid-Client/src/main.jsx
@@ -1,17 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { UploadThingProvider } from '@uploadthing/react'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UploadThingProvider
-      config={{
-        url: "https://jobgrid-d5gg.onrender.com/api/uploadthing",
-      }}
-    >
-      <App />
-    </UploadThingProvider>
+    <App />
   </StrictMode>,
 )
