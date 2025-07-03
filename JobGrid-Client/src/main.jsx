6e4c2d855cbe5f122c089359fe09c463f8e8e23d
Rahul Parihar <pariharr0203@gmail.com>
2025-07-04 03:29:58 +0530

    Removed UploadThingProvider and using direct UploadButton fallback

--- JobGrid-Client/src/main.jsx
+++ JobGrid-Client/src/main.jsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { UploadThingProvider } from "@uploadthing/react";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UploadThingProvider
-      config={{
-        uploadthingUrl: import.meta.env.PROD
-          ? "https://jobgrid-d5gg.onrender.com/api/uploadthing"
-          : "http://localhost:3000/api/uploadthing",
-      }}
-    >
-      <App />
-    </UploadThingProvider>
+    <App />
   </StrictMode>,
 )
